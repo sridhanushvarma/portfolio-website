eaad9c78e407532286ea1a7b7c899fc27a21d8fb
Sridhanush Varma <sridhanushvarmasv@gmail.com>
2025-10-29 10:06:14 +0530

    Remove nested portfolio-website directory that was causing build failures
    - The portfolio-website directory was a duplicate of the project files
    - This was created when converting from git submodule to regular directory
    - Removing it resolves the build failure in GitHub Actions

--- portfolio-website/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './Header';
-export { default as Skills } from './Skills';
-export { default as Education } from './Education';
-export { default as Projects } from './Projects';
-export { default as Section } from './Section';
-export { default as Footer } from './Footer';
